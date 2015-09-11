5845c71f97449c7647c2c884022cd7674d3be70c
AlexChow <cplover@live.com>
2015-09-12 03:09:24 +0800

    asp.net upgrade to beta7

--- src/AuthenticationServer/Startup.cs
+++ src/AuthenticationServer/Startup.cs
@@ -1,21 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Diagnostics;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Routing;
 using Microsoft.Framework.Configuration;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
-using Microsoft.Framework.Logging.Console;
-using Microsoft.Framework.Runtime;
 using BahamutService;
 using ServerControlService.Service;
-using BahamutService.Model;
 using DataLevelDefines;
+using Microsoft.Dnx.Runtime;
 
 namespace AuthenticationServer
 {
